b81eea72f5374e5a772ca0acbcad6855fe121ed6
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2023-01-21 18:39:35 +0100

    Materials (very, very basic)

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,6 @@
 #include <Pistachio.h>
 
+#include "glm/gtx/transform.hpp"
 #include "imgui/imgui.h"
 
 class ExampleLayer : public Pistachio::Layer
@@ -32,10 +33,10 @@ public:
 		m_SquareVA.reset(Pistachio::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Pistachio::VertexBuffer> squareVB;
@@ -55,14 +56,18 @@ public:
 			
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -81,44 +86,52 @@ public:
 
 		m_Shader.reset(new Pistachio::Shader(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
+
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new Pistachio::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(new Pistachio::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Pistachio::Timestep ts) override
 	{
 		if(Pistachio::Input::IsKeyPressed(PA_KEY_LEFT))
-			m_CameraPosition.x += m_CameraMoveSpeed * ts;
-		else if(Pistachio::Input::IsKeyPressed(PA_KEY_RIGHT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
+		else if(Pistachio::Input::IsKeyPressed(PA_KEY_RIGHT))
+			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 		
 		if(Pistachio::Input::IsKeyPressed(PA_KEY_UP))
-			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
-		else if(Pistachio::Input::IsKeyPressed(PA_KEY_DOWN))
 			m_CameraPosition.y += m_CameraMoveSpeed * ts;
+		else if(Pistachio::Input::IsKeyPressed(PA_KEY_DOWN))
+			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
 		if(Pistachio::Input::IsKeyPressed(PA_KEY_A))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
@@ -134,7 +147,23 @@ public:
 
 		Pistachio::Renderer::BeginScene(m_Camera);
 
-		Pistachio::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8, 0.2, 0.3, 1.0f);
+		glm::vec4 blueColor(0.2, 0.3, 0.8, 1.0f);
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				if(x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+				Pistachio::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
 		Pistachio::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Pistachio::Renderer::EndScene();
@@ -154,7 +183,7 @@ private:
 	std::shared_ptr<Pistachio::Shader> m_Shader;
 	std::shared_ptr<Pistachio::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Pistachio::Shader> m_BlueShader;
+	std::shared_ptr<Pistachio::Shader> m_FlatColorShader;
 	std::shared_ptr<Pistachio::VertexArray> m_SquareVA;
 
 	Pistachio::OrthographicCamera m_Camera;

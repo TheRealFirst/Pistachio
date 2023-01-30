dee89ced8857cf8acbaa36d54ba882ceb372f901
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2023-01-30 16:49:35 +0100

    Shader Loading from file!!!

--- Pistachio/src/Pistachio/Renderer/Shader.cpp
+++ Pistachio/src/Pistachio/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Pistachio
 {
+    Shader* Shader::Create(const std::string& filepath)
+    {
+        switch (Renderer::GetAPI())
+        {
+        case RendererAPI::API::None: PA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!") return nullptr;
+        case RendererAPI::API::OpenGL: return new OpenGLShader(filepath);
+        }
+
+        PA_CORE_ASSERT(false, "Unknown RendererAPI")
+        return nullptr;
+    }
+
     Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
     {
         switch (Renderer::GetAPI())
--- Pistachio/src/Pistachio/Renderer/Shader.h
+++ Pistachio/src/Pistachio/Renderer/Shader.h
@@ -12,6 +12,7 @@ namespace Pistachio
         virtual void Bind() const = 0;
         virtual void UnBind() const = 0;
 
+        static Shader* Create(const std::string& filepath);
         static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
     };
 }

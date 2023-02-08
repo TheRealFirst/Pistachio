444a136111b05df7a1cdade3721f15cb51abebc9
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2023-02-08 16:46:23 +0100

    now it works!!!!!

--- Pistachio/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Pistachio/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -7,19 +7,20 @@
 
 namespace Pistachio
 {
-    OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height) : m_Width(width), m_Height(height)
+    OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+        : m_Width(width), m_Height(height)
     {
         m_InternalFormat = GL_RGBA8;
-        m_DataFormat = GL_RGBA8;
+        m_DataFormat = GL_RGBA;
 
         glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-        glTextureStorage2D(m_RendererID, 1, m_DataFormat, m_Width, m_Height);
+        glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
 
         glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        glTexParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
     }
 
     OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : m_Path(path)
@@ -70,11 +71,10 @@ namespace Pistachio
 
     void OpenGLTexture2D::SetData(void* data, uint32_t size)
     {
-        uint32_t bpp = m_DataFormat == GL_RGBA8 ? 4 : 3;
-        PA_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!")
+        uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+        PA_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
         glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
     }
-
     void OpenGLTexture2D::Bind(uint32_t slot) const
     {
         glBindTextureUnit(slot, m_RendererID);
